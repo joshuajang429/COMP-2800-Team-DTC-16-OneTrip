f0172c4e29c796063b5c76b9b4354e0ec55bd812
HyungJoon <itineris24@naver.com>
2020-05-06 12:40:33 -0700

    prototype 1st finished

--- prototype/app.js
+++ prototype/app.js
@@ -10,12 +10,20 @@ const port = 3000;
 
 
 // route starts from here
-app.get("/", (req, res)=> res.render("pages/index"));
+app.get("/", (req, res) => res.render("pages/index"));
 
 app.get("/signin", (req, res) => res.render("pages/signin"));
 
 app.get("/findlocation", (req, res) => res.render("pages/findlocation"));
 
 app.get("/map", (req, res) => res.render("pages/map"));
+
+app.get("/waitingtime", (req, res) => res.render("pages/waitingtime"));
+
+app.get("/update", (req, res) => res.render("pages/update"));
+
+app.get("/marketinfo", (req, res) => res.render("pages/marketinfo"));
+
+
 
 app.listen(port);
