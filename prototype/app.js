4bf2dfc41b6940652ac6ec8bd5e266e856f343ce
HyungJoon <itineris24@naver.com>
2020-05-05 23:21:35 -0700

    frame 1,2,3

--- prototype/app.js
+++ prototype/app.js
@@ -4,11 +4,18 @@ let app = express();
     app.use(express.urlencoded({extended: true}));
     app.use(express.static("public"));
     app.set("view engine", "ejs");
+    
 
 const port = 3000;
 
 
-// routes from here
+// route starts from here
 app.get("/", (req, res)=> res.render("pages/index"));
+
+app.get("/signin", (req, res) => res.render("pages/signin"));
+
+app.get("/findlocation", (req, res) => res.render("pages/findlocation"));
+
+app.get("/map", (req, res) => res.render("pages/map"));
 
 app.listen(port);
